91e6365e187ba79e6223243e9914ce4a59fa9904
Arturo Puente <arturopuentevc@gmail.com>
2015-03-06 12:41:07 -0500

    Returns YouTube visits for a specific video

--- javascripts/dashboard.js
+++ javascripts/dashboard.js
@@ -4,11 +4,27 @@ var apiKey = "AIzaSyAqHIdFnDLnjeWa1CRBCmq879oel4KsC6w";
 
 var url = "https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=" + apiKey + "&part=snippet,contentDetails,statistics,status";
 
+var Dashboard = React.createClass({
+  displayName: "Dashboard",
+
+  render: function render() {
+    return React.createElement(
+      "div",
+      null,
+      React.createElement(
+        "h2",
+        null,
+        "Visit count"
+      ),
+      React.createElement(
+        "p",
+        null,
+        this.props.visits
+      )
+    );
+  }
+});
+
 Helpers.ajaxGet(url, function (data) {
-  console.log(data);
-  React.render(React.createElement(
-    "h1",
-    null,
-    "It works!"
-  ), document.getElementById("example"));
+  React.render(React.createElement(Dashboard, { visits: data.items[0].statistics.viewCount }), document.getElementById("example"));
 });
--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -1,11 +1,21 @@
 const apiKey = 'AIzaSyAqHIdFnDLnjeWa1CRBCmq879oel4KsC6w';
 
-var url = `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=${apiKey}&part=snippet,contentDetails,statistics,status`
+let url = `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=${apiKey}&part=snippet,contentDetails,statistics,status`
+
+let Dashboard = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h2>Visit count</h2>
+        <p>{ this.props.visits }</p>
+      </div>
+    );
+  }
+});
 
 Helpers.ajaxGet(url, function(data) {
-  console.log(data);
   React.render(
-    <h1>It works!</h1>,
-    document.getElementById('example')
+    <Dashboard visits={data.items[0].statistics.viewCount} />,
+    document.getElementById('example');
   );
 });
